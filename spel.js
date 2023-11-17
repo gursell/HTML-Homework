9cb100b8dd20de2b6cea95cb9fea41915a0600f2
gursell <unlgrsel@gmail.com>
2023-11-17 21:10:21 +0100

    17.11

--- spel.js
+++ spel.js
@@ -1,72 +1,93 @@
 const X_CLASS = 'x';
 const O_CLASS = 'o';
 const winningCombination = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-const cellElements = document.querySelectorAll('.block');
-const winningMessageElement = document.getElementById('winning-message');
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
-const resetButton = document.getElementById('restart-button');
+  // row
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // col
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // diag
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const rondText = document.getElementById('rond-text')
+const cellElements = document.querySelectorAll('[data-cell]')
+const winningMessageElement = document.getElementById('winning-message')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const resetButton = document.getElementById('restart-button')
+let ronds = 0;
 let whoTurn;
 let markArray = [];
 
 startGame();
 
-resetButton.addEventListener('click', startGame);
+resetButton.addEventListener('click', startGame)
 
 function startGame() {
-    whoTurn = false;
-    cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS);
-        cell.classList.remove(O_CLASS);
-        cell.removeEventListener('click', onClick);
-        cell.addEventListener('click', onClick);
-    });
-    winningMessageElement.classList.remove('show');
+
+  console.log("Started")
+
+  whoTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(O_CLASS)
+    cell.removeEventListener('click', onClick)
+    cell.addEventListener('click', onClick)
+  })
+  winningMessageElement.classList.remove('show')
 }
 
 function onClick(e) {
-    const cell = e.target;
-    const currentClass = whoTurn ? O_CLASS : X_CLASS;
-    placeMark(cell, currentClass);
+  const cell = e.target
+  const currentClass = whoTurn ? O_CLASS : X_CLASS;
+  if (checkCellEmpty(cell)) {
+    if (currentClass === X_CLASS) {
+      ronds++
+      rondText.innerHTML = `Rond: ${ronds}`
+    }
+    placeMarke(cell, currentClass)
+  }
+}
+
+function checkCellEmpty(cell) {
+  if (cell.classList.contains("x") || cell.classList.contains("o")) {
+    return false
+  }
+  return true
 }
 
 function endGame() {
-    winningMessageTextElement.innerText = `${whoTurn ? 'O' : 'X'} Wins!`;
-    markArray = [];
-    winningMessageElement.classList.add('show');
+  winningMessageTextElement.innerText = `${whoTurn ? "O" : "X"} Wins!`
+  ronds = 0
+  markArray = [];
+  winningMessageElement.classList.add('show')
 }
 
-function placeMark(cell, currentClass) {
-    markArray.push(cell);
-    cell.classList.add(currentClass);
-    if (checkWin(currentClass)) {
-        endGame();
-    } else {
-        swapTurns();
-        if (markArray.length > 6) {
-            const oldestMark = markArray.shift();
-            oldestMark.classList.remove(currentClass);
-        }
+function placeMarke(cell, currentClass) {
+  markArray.push(cell)
+  cell.classList.add(currentClass);
+  if (checkWin(currentClass)) {
+    endGame()
+  } else {
+    swapTurns()
+    if (markArray.length > 6) {
+      const oldestMask = markArray.shift();
+      oldestMask.classList.remove(currentClass);
     }
+  }
 }
 
 function swapTurns() {
-    whoTurn = !whoTurn;
+  whoTurn = !whoTurn
 }
 
 function checkWin(currentClass) {
-    return winningCombination.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass);
-        });
-    });
+  return winningCombination.some(cobination => {
+    return cobination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
 }
--- users.js
+++ users.js
@@ -1,4 +1,70 @@
+import fs from 'fs'
+const playerArray = []
 
+document.addEventListener('DOMContentLoaded', () => {
+  const registerButtom = document.getElementById('register')
+
+  registerButtom.addEventListener('click', register)
+})
+
+function register() {
+  console.log('1')
+  fetchData()
+  const newPlayerName = document.getElementById('player-name').value
+  console.log('3')
+  if (checkForNameExist(newPlayerName)) {
+    alert("player already existed")
+  } else {
+    console.log('5')
+    const player = new NewPlayer(newPlayerName)
+
+    playerArray.push(player.dataInfo())
+    writeToJson();
+  }
+}
+
+function fetchData() {
+  console.log('2')
+  const jsonSting = fs.readFileSync("playerList.json");
+  const data = JSON.parse(jsonSting)
+
+  for (let i = 0; i < data.lengt; i++) {
+    playerArray.push(data[i])
+  }
+}
+
+function checkForNameExist(name) {
+  console.log('4')
+  if (playerArray.some(x => x.playerName = name)) {
+    return true;
+  }
+  return false;
+}
+
+
+function writeToJson() {
+  fs.writeFileSync('./playerList.json', JSON.stringify(playerArray, null, 2), (err) => {
+    if (err) throw err;
+    console.log('data writen to file')
+  })
+}
+
+class NewPlayer {
+  constructor(name) {
+    this.name = name
+  }
+
+  dataInfo() {
+    return {
+      playerName: this.name,
+      won: 0,
+      loses: 0,
+      games: []
+    }
+  }
+}
+
+/*
 const playerArray = [];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -53,3 +119,4 @@ class NewPlayer {
     };
   }
 }
+*/
